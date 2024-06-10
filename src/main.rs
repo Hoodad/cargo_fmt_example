cd3b9db6c5062947fd61a4b79354b7fcbc7e22f9
Robin Thunstrom <robin.thunstrom@embark-studios.com>
2024-06-10 09:55:08 +0200

    Changed attributes

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 /// This text will be formatted by the cargo fmt when running, which is great and that helps a lot when you type and forget to break to a newline.
 /// ## Example
 ///
-/// ```ignore
+/// ```rust
 /// pub struct ImportantData {
 ///     // This text will however not be formatted to adhere to the max length of comments even tough it will end up on the doc page just like other comments.
 ///     some_field: u32,
+/// }
 /// ```
 
 fn main() {
